4b1f5c4183316166f6a1cbcc9d43602be0f7adf4
Shushila Budhathoki <shushila.budhathoki28@gmail.com>
2025-07-08 15:45:49 +0545

    feat: update `Input` component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,11 @@
 import { Input as AntInput, InputProps } from 'antd';
 
-export const Input = (props: InputProps) => <AntInput {...props} />;
+const InputComponent = (props: InputProps) => <AntInput {...props} />;
+
+InputComponent.Password = AntInput.Password;
+InputComponent.TextArea = AntInput.TextArea;
+InputComponent.Search = AntInput.Search;
+InputComponent.Group = AntInput.Group;
+InputComponent.OTP = AntInput.OTP;
+
+export const Input = InputComponent as typeof AntInput;
